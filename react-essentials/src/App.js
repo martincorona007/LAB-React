0de589dbd644fa325bf1f47dad84402a52ff1d6f
martincorona007 <martincorona007@hotmail.com>
2022-06-21 09:15:20 -0500

    hook useState

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-function RegularComponent(){
-  return <h1>RegularComponent</h1>
-}
-function SecretComponent(){
-  return <h1>SecretComponent</h1>
-}
+import { useState, useEffect } from 'react';
+
 function App(props) {
+  //1. the state value or state variable 
+  //2. a function to update the state
+  //useState([]) takes anything
+  const [emotion,emotionState] = useState("happy");
+
+  
+
   return(
     <>
-    {props.available ? <RegularComponent/> : <SecretComponent/>}
+      <h1>Current emotion is {emotion}. </h1>
+      <button onClick={()=> emotionState("ENGRY")}>
+        Make engry
+      </button>
+      <button onClick={()=> emotionState("Entusiastic")}>
+        Entusiastic
+      </button>
     </>
   );
-  // if(props.available){
-  //   return <RegularComponent/>;
-  // }else{
-  //   return <SecretComponent/>;
-  // }
 }
 
 export default App;
--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -9,28 +9,14 @@ function AppTwo(){
   return <h1>App two</h1>;
 }
 root.render(
-  // React.createElement("h1",{style: {color: "blue"}},"Hey dude"),//what we want to create
-  // React.createElement("h2",{style: {color: "blue"}},"Chaning colors"),
-  // <ul>
-  //   <li>d1</li>
-  //   <li>d2</li>
-  //   <li>d3</li>
-  // </ul>,
-    <App available={true}/>
-  // <div>
-  //   <AppTwo/>,
-  //   <App/>,
-  // </div>
-  //document.getElementById("root")//where to put it
+  
+    <App />
+  
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>
 );
-// ReactDOM.render(
-//   React.createElement("h1",{style: {color: "blue"}},"Hey que onda"),
-//   React.createElement("h2",null,"rendeing h2"),
-//   document.getElementById("root")
-// );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
